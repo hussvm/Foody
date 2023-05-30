24a326f2a1c6392998b41fab77f264589f7301d8
Hussam <hussvm@icloud.com>
2023-05-30 15:05:18 +0200

    Compeleting Home Page Layout

---
+++ Foody/Views/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Foody
+//
+//  Created by HUSSAM on 5/30/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
