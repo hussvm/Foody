715e8e41282a1b5b9ff84223faec1548c866214a
Hussam <hussvm@icloud.com>
2023-05-31 09:19:19 +0200

    Init and installing KingFisher Pod

---
+++ Foody/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Foody
+//
+//  Created by HUSSAM on 5/31/23.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    func setup(category: DishCategory) {
+        
+    }
+}
---
+++ Foody/Models/DishCategory.swift
@@ -0,0 +1,12 @@
+//
+//  DishCategory.swift
+//  Foody
+//
+//  Created by HUSSAM on 5/31/23.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id, name, image : String
+}
